09cb2e1e790a092d5a70a31ae5fe5f159a623cbe
AndreyGTHB <andr.g.tuk@gmail.com>
2019-03-26 16:23:03 +0700

    Control of the player using the keys.

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+from time import sleep
+
+pygame.init()
+win = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("Cubes game")
+
+x = 50
+y = 425
+width = 40
+height = 60
+speed = 5
+run = True
+while run:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > 5:
+        x -= speed
+    if keys[pygame.K_RIGHT] and x < 495 - width:
+        x += speed
+    if keys[pygame.K_UP] and y > 5:
+        y -= speed
+    if keys[pygame.K_DOWN] and y < 485 - height:
+        y += speed
+
+    win.fill((0, 0, 0))
+
+    pygame.draw.rect(win, (0, 0, 255), (x, y, width, height))
+    pygame.display.update()
+
+pygame.quit()
