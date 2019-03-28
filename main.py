b3e1b8cf70e042c019d27c8c44518a21c9044643
Grigory <grigory@clearscale.net>
2019-03-28 17:39:59 +0700

    jump was added

--- main.py
+++ main.py
@@ -6,14 +6,39 @@ win = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("Cubes game")
 
+walkRight = [pygame.image.load('res\pygame_right_1.png'),
+             pygame.image.load('res\pygame_right_2.png'),
+             pygame.image.load('res\pygame_right_3.png'),
+             pygame.image.load('res\pygame_right_4.png'),
+             pygame.image.load('res\pygame_right_5.png'),
+             pygame.image.load('res\pygame_right_6.png')]
+
+walkLeft = [pygame.image.load('res\pygame_left_1.png'),
+            pygame.image.load('res\pygame_left_2.png'),
+            pygame.image.load('res\pygame_left_3.png'),
+            pygame.image.load('res\pygame_left_4.png'),
+            pygame.image.load('res\pygame_left_5.png'),
+            pygame.image.load('res\pygame_left_6.png')]
+
+bg = pygame.image.load('res\pygame_bg.jpg')
+playerStand = pygame.image.load('res\image.png')
+
 x = 50
 y = 425
-width = 40
-height = 60
+width = 60
+height = 71 
 speed = 5
+
+isJump = False
+jumpCount = 10
+
+left = False
+right = False
+animCount = 0
+
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -23,14 +48,18 @@ while run:
         x -= speed
     if keys[pygame.K_RIGHT] and x < 495 - width:
         x += speed
-    if keys[pygame.K_UP] and y > 5:
-        y -= speed
-    if keys[pygame.K_DOWN] and y < 485 - height:
-        y += speed
 
-    win.fill((0, 0, 0))
-
-    pygame.draw.rect(win, (0, 0, 255), (x, y, width, height))
-    pygame.display.update()
+    if not isJump:
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    elif jumpCount >= -10:
+        if jumpCount < 0:
+            y += (jumpCount ** 2) / 2
+        else:
+            y -= (jumpCount ** 2) / 2
+        jumpCount -= 1
+    else:
+        isJump = False
+        jumpCount = 10
 
 pygame.quit()
